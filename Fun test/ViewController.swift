add0625ef75cb5a7f4dd68d3cd92e8935c40cd9e
Razvan Tache <rtache@tremend.ro>
2018-01-23 08:03:34 +0200

    Tutorial: Add basic elements to projects and remove comments/dead code

--- Fun test/ViewController.swift
+++ Fun test/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Fun test
-//
-//  Created by Razvan Tache on 20/01/2018.
-//  Copyright © 2018 Razvan Tache. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,12 +6,4 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
